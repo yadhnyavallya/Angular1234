77a9436539c8acab77da08ae610d19b645dfac10
techanicalguftgu <yadhnyavallyag@gmail.com>
2023-07-20 12:52:59 +0530

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './Container/container.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { NotificationComponent } from './notification/notification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    NavComponent,
+    HeaderComponent,
+    NotificationComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/notification/notification.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  template: '<div><p>welcome to Angular Notifaction Template url</p></div>',
+  styles: ["div{margin:10px 0px; padding:10px 20px; background-color:#FAD7A0; text-align:center;}","p{font-size:14px;}"]
+})
+export class NotificationComponent {
+
+}
